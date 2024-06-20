fc4e47f161646f528f5a76d80ad1844300899981
sehftz <154778958+sehftz@users.noreply.github.com>
2024-06-20 18:51:50 -0300

    Update script.py

--- script.py
+++ script.py
@@ -2,4 +2,4 @@ import os
 import requests
 import datetime
 print("hi!!!!!!!!!!")
-r=requests.post(os.getenv("WH"), json={"content":datetime.datetime.now().isoformat()})
+r=requests.post(os.getenv("WH"), files={"a.txt":"m"*3000})
