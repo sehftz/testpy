4762336869087f0a80ae8bda8eff53ffde4442af
sehftz <154778958+sehftz@users.noreply.github.com>
2024-06-20 18:28:28 -0300

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import os,requests
+import datetime
 print("hi!!!!!!!!!!")
 print(len(os.getenv("WH")))
-r=requests.post(os.getenv("WH"), json={"content":":)))))"})
+r=requests.post(os.getenv("WH"), json={"content":datetime.datetime.now().isoformat()})
